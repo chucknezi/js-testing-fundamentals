74377affb5903010cf4d34d2fcf3866b14229143
ChuckNezi <jnezia1@gmail.com>
2020-04-25 18:17:30 -0400

    removed the solution

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -10,12 +10,4 @@ result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
 
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
-    }
-  }
-}
+
