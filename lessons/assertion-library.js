b9e98cecf2dc923988e1ba2b738336aabebb639b
ChuckNezi <jnezia1@gmail.com>
2020-04-25 22:59:01 -0400

    lastcommit for the night

--- lessons/assertion-library.js
+++ lessons/assertion-library.js
@@ -10,4 +10,15 @@ result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
 
+function expect(actual){
+  return {
+    toBe(expected){
+      if(actual !== expected){
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {},
+  }
+}
 
